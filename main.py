049347f3f4640207b3a7fe72c5f5974d5daa7742
Edinson Gutierrez <edison.gutierrez-ext@ab-inbev.com>
2026-04-11 22:15:24 -0500

    feat: apply filter in project

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from news_analyzer.exceptions import APIKeyError
 from news_analyzer.api_client import fetch_news
 from news_analyzer.config import API_KEY
-from news_analyzer.utils import get_unique_sources
+from news_analyzer.utils import get_unique_sources, get_articles_by_sources
 
 from dotenv import load_dotenv
 load_dotenv()  # Carga las variables del archivo .env
@@ -19,3 +19,7 @@ if response_data:
     sources_set: set = get_unique_sources(response_data.get("articles", []))
     for index, source in enumerate(sources_set, start=1):
         print(f"No: {index} -- { source}")
+
+    xataka_articles: list[dict] = get_articles_by_sources(response_data.get("articles"), "github.io")
+    for xataka_article in xataka_articles:
+        print(f"""{xataka_article["source"]["name"]} - {xataka_article["title"]}""")
--- news_analyzer/utils.py
+++ news_analyzer/utils.py
@@ -13,3 +13,19 @@ def get_unique_sources(articles: list[dict]) -> set:
         for article in articles
         if article.get("source") and article.get("source").get("name")    
     }
+
+
+def get_articles_by_sources(articles: list[dict], source: str) -> list[dict]:
+    """Filter articles by a specific news source.
+    Args:
+        articles (list[dict]): A list of news articles, each containing a 'source' key.
+        source (str): The name of the news source to filter by.
+    Returns:
+        list[dict]: A list of articles that match the specified news source.
+    """
+    return list(
+        filter(
+            lambda article: article["source"]["name"].lower() == source.lower(),
+            articles
+        )
+    )
